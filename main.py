bf3962f3a1a5c2a1d3865e2008dda0496880cb86
Malcolm Diller <malcolm@dillerm.io>
2021-12-12 19:44:19 -0800

    added some changes from a long time go

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import os
 import json
+import time
+# from heroes import get_hero_infos
+from collections import Counter
 
 cache_dir = "cache"
 if not os.path.exists(cache_dir):
@@ -27,79 +30,101 @@ def do_web_request(url, cache_name):
 	if data:
 		return data
 	response = requests.get(url)
-	if response.status_code != 200:
+	if response.status_code == 429:
+		seconds_to_wait = 10
+		print(f"being rate limited, waiting {seconds_to_wait} seconds...")
+		time.sleep(seconds_to_wait)
+		return do_web_request(url, cache_name)
+	elif response.status_code != 200:
+		print(response.headers)
 		raise Exception(f"shit broke: {response.status_code}")
 	data = response.json()
 	save_cache(cache_name, data)
 	return data
 
-def ranktostars(rank):
-	if rank is None:
-		return None
-	stars = rank % 10
-	medal = rank // 10
-	return (medal * 7) + (stars - 1)
+def create_hero_grid_config(categories):
+	hero_grid = {
+		"config_name": "Most Played",
+		"categories": []
+	}
+	names = {
+		"safe": "Safe Lane",
+		"mid": "Mid Lane",
+		"off": "Offlane",
+		"support": "Support"
+	}
 
-medal_strings = [ "Unranked", "Herald", "Guardian", "Crusader", "Archon", "Legend", "Ancient", "Divine", "Immortal" ]
+	for category in categories:
+		hero_counts = sorted(Counter(categories[category]).items(), key=lambda x: x[1], reverse=True)
+		cat_info = {
+			"category_name": names[category],
+			"x_position": 0,
+			"y_position": 0,
+			"width": 0,
+			"height": 0,
+			"hero_ids": []
+		}
+		for hero_count in hero_counts:
+			cat_info["hero_ids"].append(hero_count[0])
+		hero_grid["categories"].append(cat_info)
 
-def avg(items):
-	n = 0
-	for item in items:
-		n += item
-	return n / len(items)
+	max_width = 1000
+	max_height = 550
+	gap = 0
+	num_categories = len(hero_grid["categories"])
+	category_height = (max_height / num_categories) - gap
 
-min_ranks = 3
+	for i in range(0, num_categories):
+		cat = hero_grid["categories"][i]
+		cat["x_position"] = 0
+		cat["y_position"] = (i * category_height) + (i * gap)
+		cat["width"] = max_width
+		cat["height"] = category_height
 
-# gets a rank string from its stars value
-def rankstring(rank):
-	medal = medal_strings[rank // 7]
-	stars = (rank % 7) + 1
-	return f"{medal} [{stars}]"
+	return {
+		"version": 3,
+		"configs": [
+			hero_grid
+		]
+	}
 
-def rankdiff(team1, team2, printall=False):
-	def getrank(player):
-		return ranktostars(player.get("rank_tier"))
-	team1 = map(getrank, team1)
-	team2 = map(getrank, team2)
-	team1 = list(filter(lambda i: i is not None, team1))
-	team2 = list(filter(lambda i: i is not None, team2))
-	if len(team1) < min_ranks or len(team2) < min_ranks:
-		if printall:
-			print(f"team1: {len(team1)} ranked players")
-			print(f"team2: {len(team2)} ranked players")
-		return None
-	if printall:
-		print("\nteam 1")
-		for rank in team1:
-			print(f"({rank}) {rankstring(rank)}")
-		print(f"average:    ({avg(team1)}) {rankstring(int(avg(team1)))}")
-		print("\nteam 2")
-		for rank in team2:
-			print(f"({rank}) {rankstring(rank)}")
-		print(f"average:    ({avg(team2)}) {rankstring(int(avg(team2)))}")
-	return avg(team1) - avg(team2)
 
 # prints the rankdiff for a single match
-def singlematch(steamid, match_id):
+def singlematch_addhero(steamid, match_id, categories):
 	match = do_web_request(f"http://api.opendota.com/api/matches/{match_id}", f"match_{match_id}")
 	players = match["players"]
 	radiant = list(filter(lambda p: p["player_slot"] < 128, players))
 	dire = list(filter(lambda p: p["player_slot"] >= 128, players))
 
-	is_radiant = any(p["account_id"] == steamid for p in radiant)
+	player = None
+	for p in players:
+		if p["account_id"] == steamid:
+			player = p
 
-	if is_radiant:
-		team1 = radiant
-		team2 = dire
+	if player is None:
+		return #skip
+
+
+	is_radiant = player["player_slot"] < 128
+
+	team = radiant if is_radiant else dire
+
+	lanes = {1: "safe", 2: "mid", 3: "off"}
+	gold_key = "lane_efficiency"
+
+	if "lane_role" not in player:
+		return # skip this match as it hasnt been parsed
+
+	hero_id = player["hero_id"]
+	lane_role = player["lane_role"]
+	team_lane = list(filter(lambda p: p["lane_role"] == lane_role, team))
+
+	if any(p[gold_key] > player[gold_key] for p in team_lane):
+		categories["support"].append(hero_id)
 	else:
-		team1 = dire
-		team2 = radiant
-
-	diff = rankdiff(team1, team2, True)
-	if diff is None:
-		print("not enough ranked players to show a diff")
-		return
-	print(f"diff: {diff:.2f}")
+		if hero_id == 119:
+			print(match_id)
+		categories[lanes[lane_role]].append(hero_id)
 
 def main(steamid, days, queryargs=None):
 	url = f"http://api.opendota.com/api/players/{steamid}/matches?date={days}"
@@ -120,54 +145,28 @@ def main(steamid, days, queryargs=None):
 
 	matches.reverse()
 
-	csv_data = ""
+	categories = {
+		"safe": [],
+		"mid": [],
+		"off": [],
+		"support": []
+	}
 
 	for match in matches:
-		players = match["players"]
-		radiant = list(filter(lambda p: p["player_slot"] < 128, players))
-		dire = list(filter(lambda p: p["player_slot"] >= 128, players))
+		singlematch_addhero(steamid, match["match_id"], categories)
 
-		is_radiant = any(p["account_id"] == steamid for p in radiant)
+	# hero_infos = get_hero_infos()
 
-		if is_radiant:
-			team1 = radiant
-			team2 = dire
-		else:
-			team1 = dire
-			team2 = radiant
+	for category in categories:
+		print(f"{category}:")
+		hero_counts = sorted(Counter(categories[category]).items(), key=lambda x: x[1], reverse=True)
+		for hero_count in hero_counts:
+			print(" - " + str(hero_count[0]) + f" ({hero_count[1]})")
 
-		diff = rankdiff(team1, team2)
-		if diff is not None:
-			print(f"match {match['match_id']: <10} diff: {' ' if diff > 0 else ''}{diff:.2f}")
-			csv_data += f"{match['match_id']},{diff:.2f}\n"
-			alldiffs.append(diff)
-		else:
-			print(f"match {match['match_id']: <10} skipped")
-
-
-	with open("out.csv", "w+") as f:
-		f.write(csv_data)
-
-
-	print(f"average: {avg(alldiffs):.2f}")
+	grid_config = create_hero_grid_config(categories)
+	with open("hero_grid_config.json", "w+") as f:
+		f.write(json.dumps(grid_config, indent="\t"))
 
 
 # all
-# main(95211699, 60)
-
-# ranked
-# main(95211699, 60, "lobby_type=7")
-
-# unranked
-# main(95211699, 60, "lobby_type=0")
-
-# with teammate
-# main(95211699, 60, "included_account_id=170801607")
-
-# ranked w/ teammate
-main(95211699, 60, "included_account_id=170801607&lobby_type=7")
-
-# singlematch(95211699, 4997722586)
-
-
-# singlematch(95211699, 5004860090)
+main(185742631, 120)
